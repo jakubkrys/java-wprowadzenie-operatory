0f528f61ee640587dfe1847c38df7344dfa8b12f
Jakub <jakubkrys@gmail.com>
2020-12-06 16:41:15 +0100

    #3 Program liczący obwód koła przy podanym promieniu

---
+++ Circuit.java
@@ -0,0 +1,17 @@
+class Circuit {
+	public static void main(String args[]){
+		
+		byte r = 7;
+		double pi = 3.14;
+		double circleCircuit;
+		
+		circleCircuit = 2*pi*r;
+		System.out.println();
+		System.out.println("-----Circle Circuit-----");
+		System.out.println();
+		System.out.println("Radius: "+r);
+		System.out.println("Circle Circuit: "+circleCircuit);
+		System.out.println();
+		
+	}
+}
