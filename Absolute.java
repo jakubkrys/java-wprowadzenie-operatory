80c47bc085fdf9befebd43d0d290dfa478c75dda
Jakub <jakubkrys@gmail.com>
2020-12-06 21:15:09 +0100

    #11 Program liczący wartość absolutną dla zmiennej

---
+++ Absolute.java
@@ -0,0 +1,12 @@
+class Absolute {
+	public static void main(String args[]){
+		int x = -11490;
+		int absoluteValue;
+		
+		absoluteValue = x >= 0 ? x : -x;
+		
+		System.out.println("Absolute value: "+absoluteValue); 
+		
+		
+	}
+}
